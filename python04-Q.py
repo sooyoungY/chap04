cea8669ce30600615ab73438efe25143f732cc28
tndud9858 <tndud9858@gmail.com>
2019-02-28 17:50:38 +0900

    4장 연습문제

--- python04-Q.py
+++ python04-Q.py
@@ -17,38 +17,38 @@ for n in range(0, 11):
     print("{0},".format(fib(n)), end="")
 print("\n")
 
-print("연습문제 2)")
+# print("연습문제 2)")
 
-count = 0
+# count = 0
 
-f = open("C:/SooYoung(don't delete)/Python/chap04/sample.txt", "r", encoding="utf8")
-datas = f.readlines()
-for data in datas:
-    count += int(data)
-    aver = count/len(datas)
-f.close()
+# f = open("C:/SooYoung(don't delete)/Python/chap04/sample.txt", "r", encoding="utf8")
+# datas = f.readlines()
+# for data in datas:
+#     count += int(data)
+#     aver = count/len(datas)
+# f.close()
 
-print(count)
-print(aver)
+# print(count)
+# print(aver)
 
-f = open("C:/SooYoung(don't delete)/Python/chap04/sample.txt", "r", encoding="utf8")
-lines = f.readlines()
-f.close()
+# f = open("C:/SooYoung(don't delete)/Python/chap04/sample.txt", "r", encoding="utf8")
+# lines = f.readlines()
+# f.close()
 
-total = 0
-for line in lines:
-    score = int(line)
-    total += score
+# total = 0
+# for line in lines:
+#     score = int(line)
+#     total += score
 
-average = total / len(lines)
+# average = total / len(lines)
 
-print(total)
-print(average)
+# print(total)
+# print(average)
 
-f = open("result.txt", "w")
+# f = open("result.txt", "w")
 
-f.write(str(average))
-f.close
+# f.write(str(average))
+# f.close
 
 # print()
 # print("연습문제 3)")
@@ -71,35 +71,93 @@ f.close
 #     print(val)
 
 
+# print()
+# print("문제 4)")
+
+# def memoryWrite():
+#     f = open("memory.txt", "a", encoding="utf8")
+#     while True:
+#         w = input("")
+#         if not w:
+#             f.close()
+#             break
+#         f.write(w + "\n")
+
+# def memoryRead():    
+#     f = open("memory.txt", "r", encoding="utf8")
+#     line = f.readlines()
+#     f.close()
+#     for val in line:
+#         print(val)
+
+# while 1:
+#     print("메뉴 | 1. 내용입력 | 2. 내용출력 | 3. 종료")
+#     num = input("입력하세요 : ")
+#     num = int(num)
+#     if num == 1:
+#         memoryWrite()
+#     elif num == 2:
+#         memoryRead()
+#     elif num == 3:
+#         print("프로그램 종료합니다.")
+#         break
+#     else:
+#         print("잘못된 입력입니다. \n")
+
+# 성적표 출력 함수를 사용하여 프로그램 제작
+# 5점에 한단계식 낮아짐
+# 96점 이상 A+ ...
+# 60점 미만은 F
+
+# score = int(input("성적을 입력 하세요 : "))
+
+# if score > 95:
+#     print("성적은 {0}이며 등급은 {1} 입니다.".format(score, "A+"))
+# elif score > 89:
+#     print("성적은 {0}이며 등급은 {1} 입니다.".format(score, "A"))
+# elif score > 84:
+#     print("성적은 {0}이며 등급은 {1} 입니다.".format(score, "B+"))
+# elif score > 79:
+#     print("성적은 {0}이며 등급은 {1} 입니다.".format(score, "B"))
+# elif score > 74:
+#     print("성적은 {0}이며 등급은 {1} 입니다.".format(score, "C+"))
+# elif score > 69:
+#     print("성적은 {0}이며 등급은 {1} 입니다.".format(score, "C"))
+# elif score > 64:
+#     print("성적은 {0}이며 등급은 {1} 입니다.".format(score, "D+"))
+# elif score > 59:
+#     print("성적은 {0}이며 등급은 {1} 입니다.".format(score, "D"))
+# else:
+#     print("성적은 {0}이며 등급은 {1} 입니다.".format(score, "F"))
+
+
 print()
-print("문제 4)")
 
-def memoryWrite():
-    f = open("memory.txt", "a", encoding="utf8")
-    while True:
-        w = input("")
-        if not w:
-            f.close()
-            break
-        f.write(w + "\n")
+def scorePrint(score, level):
+    print("성적은 {0}이며 등급은 {1} 입니다.".format(score, level))
 
-def memoryRead():    
-    f = open("memory.txt", "r", encoding="utf8")
-    line = f.readlines()
-    f.close()
-    for val in line:
-        print(val)
-
-while 1:
-    print("메뉴 | 1. 내용입력 | 2. 내용출력 | 3. 종료")
-    num = input("입력하세요 : ")
-    num = int(num)
-    if num == 1:
-        memoryWrite()
-    elif num == 2:
-        memoryRead()
-    elif num == 3:
-        print("프로그램 종료합니다. \n")
-        break
+def selectScore(score):
+    if score > 95:
+        scorePrint(score, "A+")
+    elif score > 89:
+        scorePrint(score, "A")
+    elif score > 84:
+        scorePrint(score, "B+")
+    elif score > 79:
+        scorePrint(score, "B")
+    elif score > 74:
+        scorePrint(score, "C+")
+    elif score > 69:
+        scorePrint(score, "C")
+    elif score > 64:
+        scorePrint(score, "D+")
+    elif score > 59:
+        scorePrint(score, "D")
     else:
-        print("잘못된 입력입니다. \n")
+        scorePrint(score, "F")
+
+def inputScore():
+    score = int(input("성적을 입력 하세요 : "))
+    return score
+
+selectScore(inputScore())
